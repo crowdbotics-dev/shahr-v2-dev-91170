ebe8fbbd1c3f13e3f8d7cac0b0f5a1b2e7cef997
Sharaiz Ali <shahraiz.ali@crowdbotics.com>
2023-06-21 20:01:32 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/shahrvdevAPI/api.js
+++ store/shahrvdevAPI/api.js
@@ -1,73 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const shahrvdevAPI = axios.create({
   baseURL: "https://shahr-v2-dev-91170.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return shahrvdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return shahrvdevAPI.post(`/api/v1/login/`, payload.data)
+  return shahrvdevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_pet_list(payload) {
-  return shahrvdevAPI.get(`/api/v1/pet/`)
+  return shahrvdevAPI.get(`/api/v1/pet/`);
 }
+
 function api_v1_pet_create(payload) {
-  return shahrvdevAPI.post(`/api/v1/pet/`, payload.data)
+  return shahrvdevAPI.post(`/api/v1/pet/`, payload.data);
 }
+
 function api_v1_pet_retrieve(payload) {
-  return shahrvdevAPI.get(`/api/v1/pet/${payload.id}/`)
+  return shahrvdevAPI.get(`/api/v1/pet/${payload.id}/`);
 }
+
 function api_v1_pet_update(payload) {
-  return shahrvdevAPI.put(`/api/v1/pet/${payload.id}/`, payload.data)
+  return shahrvdevAPI.put(`/api/v1/pet/${payload.id}/`, payload.data);
 }
+
 function api_v1_pet_partial_update(payload) {
-  return shahrvdevAPI.patch(`/api/v1/pet/${payload.id}/`, payload.data)
+  return shahrvdevAPI.patch(`/api/v1/pet/${payload.id}/`, payload.data);
 }
+
 function api_v1_pet_destroy(payload) {
-  return shahrvdevAPI.delete(`/api/v1/pet/${payload.id}/`)
+  return shahrvdevAPI.delete(`/api/v1/pet/${payload.id}/`);
 }
+
 function api_v1_signup_create(payload) {
-  return shahrvdevAPI.post(`/api/v1/signup/`, payload.data)
+  return shahrvdevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/login/`, payload.data)
+  return shahrvdevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return shahrvdevAPI.get(`/rest-auth/logout/`)
+  return shahrvdevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/logout/`)
+  return shahrvdevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return shahrvdevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return shahrvdevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return shahrvdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return shahrvdevAPI.post(`/rest-auth/registration/`, payload.data)
+  return shahrvdevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return shahrvdevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return shahrvdevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return shahrvdevAPI.get(`/rest-auth/user/`)
+  return shahrvdevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return shahrvdevAPI.put(`/rest-auth/user/`, payload.data)
+  return shahrvdevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return shahrvdevAPI.patch(`/rest-auth/user/`, payload.data)
+  return shahrvdevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -89,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
